eb47b736bb037392b0aaf9ced709a04eff97c6af
Hanna922 <hanna.urssu@gmail.com>
2024-05-30 23:38:12 +0900

    config: setting tsup in yls
    - 환경 변수 관련 이슈 추후 해결할 예정

--- packages/logging-system/src/apis/customedAxios.ts
+++ packages/logging-system/src/apis/customedAxios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const customedAxios = axios.create({
-  baseURL: import.meta.env.VITE_API_YLS_URL,
+  // baseURL: import.meta.env.VITE_API_YLS_URL,
+  baseURL: '',
   headers: {
     'Content-Type': 'application/json',
     withCredentials: true,
