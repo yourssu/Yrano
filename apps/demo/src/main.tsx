d68a3d42afc030feedd8dc0b950f449e25729c44
mj <minai621@naver.com>
2024-06-03 11:42:34 +0900

    feat: demo에서 Context를 window 객체로 변경

--- apps/demo/src/main.tsx
+++ apps/demo/src/main.tsx
@@ -1,12 +1,11 @@
-import { YLSWrapper } from '@yourssu/logging-system-react';
 import ReactDOM from 'react-dom/client';
 
 import { App } from './App';
 
 const baseURL = import.meta.env.VITE_API_YLS_URL;
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <YLSWrapper baseURL={baseURL}>
-    <App />
-  </YLSWrapper>
-);
+window.YLS_CONFIG = {
+  baseURL,
+};
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
--- apps/demo/src/vite-env.d.ts
+++ apps/demo/src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+declare global {
+  interface Window {
+    YLS_CONFIG: {
+      baseURL: string;
+    };
+  }
+}
