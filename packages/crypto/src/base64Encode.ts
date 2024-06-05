7defb62619d78b84aaf9605e5c577f0cb09154a8
심민성 <bighammer1753@gmail.com>
2024-06-05 13:28:08 +0900

    refactor: Buffer를 사용하지 않고 base64 encoding 구현

--- packages/crypto/src/base64Encode.ts
+++ packages/crypto/src/base64Encode.ts
@@ -1,3 +1,26 @@
-export function base64Encode(str: string): string {
-  return Buffer.from(str).toString('base64');
+export function base64Encode(data: string): string {
+  const base64Chars: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+  let bins: string = '';
+
+  for (let i = 0; i < data.length; i++) {
+    const charCode: number = data.charCodeAt(i);
+    const binary: string = charCode.toString(2).padStart(8, '0');
+    bins += binary;
+  }
+
+  if (bins.length % 6 !== 0) {
+    bins += '0'.repeat(6 - (bins.length % 6));
+  }
+  let idx: number = 0;
+  let ret: string = '';
+
+  while (idx < bins.length) {
+    ret += base64Chars[parseInt(bins.slice(idx, idx + 6), 2)];
+    idx += 6;
+  }
+
+  if (ret.length % 4 !== 0) {
+    ret += '='.repeat(4 - (ret.length % 4));
+  }
+  return ret;
 }
