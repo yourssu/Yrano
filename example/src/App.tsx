6cd2e235bf12d976fdd8431c73010a71194765ef
intersoom <sallybig@naver.com>
2024-06-07 03:46:30 +0900

    feat : add example

---
+++ example/src/App.tsx
@@ -0,0 +1,13 @@
+// import { useEffect, useState } from 'react';
+
+const App = () => {
+  // eslint-config 동작 검증을 위해서 주석을 풀어주세요 !
+
+  // const [count, setCount] = useState(0);
+  // useEffect(() => {
+  //   console.log(count);
+  // }, []);
+  return <></>;
+};
+
+export default App;
---
+++ example/vite.config.ts
@@ -0,0 +1,8 @@
+/* eslint-disable */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+});
