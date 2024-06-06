a22188534d113ffd866b3a5dcea5dde653d3a68c
심민성 <bighammer1753@gmail.com>
2024-06-06 22:46:50 +0900

    test: test 방식 CryptoJS를 사용한 값과 비교 하는 것으로 변경

--- packages/crypto/src/base64Encode.test.ts
+++ packages/crypto/src/base64Encode.test.ts
@@ -1,10 +1,13 @@
+import CryptoJS from 'crypto-js';
 import { describe, it, expect } from 'vitest';
 
 import { base64Encode } from './base64Encode';
 
 describe('base64Encode', () => {
   it('should encode a string to base64', () => {
-    expect(base64Encode('hello world')).toBe('aGVsbG8gd29ybGQ=');
+    expect(base64Encode('hello world')).toBe(
+      CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse('hello world')).toString()
+    );
   });
   it('should encode a very long string to base64', () => {
     expect(
@@ -12,7 +15,11 @@ describe('base64Encode', () => {
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
       )
     ).toBe(
-      'TG9yZW0gaXBzdW0gZG9sb3Igc2l0IGFtZXQsIGNvbnNlY3RldHVyIGFkaXBpc2NpbmcgZWxpdCwgc2VkIGRvIGVpdXNtb2QgdGVtcG9yIGluY2lkaWR1bnQgdXQgbGFib3JlIGV0IGRvbG9yZSBtYWduYSBhbGlxdWEuIFV0IGVuaW0gYWQgbWluaW0gdmVuaWFtLCBxdWlzIG5vc3RydWQgZXhlcmNpdGF0aW9uIHVsbGFtY28gbGFib3JpcyBuaXNpIHV0IGFsaXF1aXAgZXggZWEgY29tbW9kbyBjb25zZXF1YXQuIER1aXMgYXV0ZSBpcnVyZSBkb2xvciBpbiByZXByZWhlbmRlcml0IGluIHZvbHVwdGF0ZSB2ZWxpdCBlc3NlIGNpbGx1bSBkb2xvcmUgZXUgZnVnaWF0IG51bGxhIHBhcmlhdHVyLiBFeGNlcHRldXIgc2ludCBvY2NhZWNhdCBjdXBpZGF0YXQgbm9uIHByb2lkZW50LCBzdW50IGluIGN1bHBhIHF1aSBvZmZpY2lhIGRlc2VydW50IG1vbGxpdCBhbmltIGlkIGVzdCBsYWJvcnVtLg=='
+      CryptoJS.enc.Base64.stringify(
+        CryptoJS.enc.Utf8.parse(
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        )
+      ).toString()
     );
   });
 });
--- packages/crypto/src/encrypt.test.ts
+++ packages/crypto/src/encrypt.test.ts
@@ -1,21 +1,22 @@
+import CryptoJS from 'crypto-js';
 import { describe, it, expect } from 'vitest';
 
 import { encrypt } from './encrypt';
 
 describe('encrypt', () => {
   it('should return the correct hash for a string', () => {
-    expect(encrypt('hello')).toBe(
-      '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824'
-    );
+    expect(encrypt('hello')).toBe(CryptoJS.SHA256('hello').toString(CryptoJS.enc.Hex));
   });
 
   it('should return the correct hash for an empty string', () => {
-    expect(encrypt('')).toBe('e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855');
+    expect(encrypt('')).toBe(CryptoJS.SHA256('').toString(CryptoJS.enc.Hex));
   });
 
   it('should return the correct hash for a long string', () => {
     expect(encrypt('Lorem ipsum dolor sit amet, consectetur adipiscing elit.')).toBe(
-      'a58dd8680234c1f8cc2ef2b325a43733605a7f16f288e072de8eae81fd8d6433'
+      CryptoJS.SHA256('Lorem ipsum dolor sit amet, consectetur adipiscing elit.').toString(
+        CryptoJS.enc.Hex
+      )
     );
   });
 
@@ -24,12 +25,16 @@ describe('encrypt', () => {
       encrypt(
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
       )
-    ).toBe('a190e90a2a184c077b7fbf9dbc3fd71c07de809b93485538580b5d075b9e466c');
+    ).toBe(
+      CryptoJS.SHA256(
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+      ).toString(CryptoJS.enc.Hex)
+    );
   });
 
   it('should return the correct hash for a string with special characters', () => {
     expect(encrypt('!@#@#s@W$$)#%(#%)_%#*($*!&(*&@#__(@#$(+%(')).toBe(
-      '335efcb347be1921fdd906802d0aa06c81c187c7ffde86f7bbc760441daca7c4'
+      CryptoJS.SHA256('!@#@#s@W$$)#%(#%)_%#*($*!&(*&@#__(@#$(+%(').toString(CryptoJS.enc.Hex)
     );
   });
 });
