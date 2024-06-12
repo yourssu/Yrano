fcef54b5d57c4f73986989b027af2db40ec41829
심민성 <bighammer1753@gmail.com>
2024-06-13 00:27:14 +0900

    feat: nextra initialization

---
+++ apps/docs/theme.config.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { DocsThemeConfig } from 'nextra-theme-docs';
+
+const config: DocsThemeConfig = {
+  logo: <span>My Project</span>,
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+};
+
+export default config;
