f1a75307f631563450b9729035666b2f275726f1
Hanna922 <hanna.urssu@gmail.com>
2024-06-13 11:09:07 +0900

    refactor: add fail test code

--- packages/crypto/src/base64Encode.test.ts
+++ packages/crypto/src/base64Encode.test.ts
@@ -2,6 +2,7 @@ import CryptoJS from 'crypto-js';
 import { describe, it, expect } from 'vitest';
 
 import { base64Encode } from './base64Encode';
+import { sha256 } from './sha256';
 
 describe('base64Encode', () => {
   it('should encode a string to base64', () => {
@@ -20,6 +21,20 @@ describe('base64Encode', () => {
         CryptoJS.enc.Utf8.parse(
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         )
+      ).toString()
+    );
+  });
+
+  it('should encode an empty sha256 hash value to base64', () => {
+    expect(base64Encode(sha256(''))).toBe(
+      CryptoJS.enc.Base64.stringify(CryptoJS.SHA256('')).toString()
+    );
+  });
+
+  it('should encode a sha256 hash value to base64', () => {
+    expect(base64Encode('9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08')).toBe(
+      CryptoJS.enc.Base64.stringify(
+        CryptoJS.SHA256('9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08')
       ).toString()
     );
   });
