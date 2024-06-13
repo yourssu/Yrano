6281326719e1b294432ee87956f2c5020fc1ea9b
심민성 <75308777+owl1753@users.noreply.github.com>
2024-06-13 22:58:33 +0900

    fix(@yourssu/crypto): invalid base64 encoding when using sha256 (#33)
    * refactor: add fail test code
    
    * feat: hexToUtf8 함수 추가
    
    * test: base64Encode test 코드 @yourssu/logging-system과 같이 변경
    
    * fix: code formatting
    
    * fix: export hexToUtf8 function
    
    * create perfect-jars-agree
    
    ---------
    
    Co-authored-by: Hanna922 <hanna.urssu@gmail.com>

--- packages/crypto/src/base64Encode.test.ts
+++ packages/crypto/src/base64Encode.test.ts
@@ -2,6 +2,8 @@ import CryptoJS from 'crypto-js';
 import { describe, it, expect } from 'vitest';
 
 import { base64Encode } from './base64Encode';
+import { hexToUtf8 } from './hexToUtf8.ts';
+import { sha256 } from './sha256';
 
 describe('base64Encode', () => {
   it('should encode a string to base64', () => {
@@ -21,6 +23,18 @@ describe('base64Encode', () => {
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         )
       ).toString()
+    );
+  });
+
+  it('should encode an empty sha256 hash value to base64', () => {
+    expect(base64Encode(hexToUtf8(sha256('')))).toBe(
+      CryptoJS.SHA256('').toString(CryptoJS.enc.Base64)
+    );
+  });
+
+  it('should encode a sha256 hash value to base64', () => {
+    expect(base64Encode(hexToUtf8(sha256('test')))).toBe(
+      CryptoJS.SHA256('test').toString(CryptoJS.enc.Base64)
     );
   });
 });
