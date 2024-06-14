f99851845f586771013a38440de51bca4a4725cd
intersoom <sallybig@naver.com>
2024-06-15 01:45:56 +0900

    refactor: move apps/example/config-example -> apps/config-example

--- apps/config-example/src/main.tsx
+++ apps/config-example/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import ReactDOM from 'react-dom/client';
 
-import App from './App.tsx';
+import App from './App.js';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
