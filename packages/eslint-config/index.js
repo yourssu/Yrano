066c25def859b7682efde77c102b3392acc265d8
intersoom <sallybig@naver.com>
2024-06-14 16:37:26 +0900

    feat: add '@typescript-eslint/no-explicit-any': 'warn'

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -12,6 +12,8 @@ module.exports = {
     'prettier/prettier': ['error', { endOfLine: 'auto' }],
     // return type 명시했는지 체크 (TS 내에서 React.FC 사용 지양을 위해서 off)
     '@typescript-eslint/explicit-function-return-type': 'off',
+    // any를 사용해야하는 특수한 경우를 위해서 warn으로 설정
+    '@typescript-eslint/no-explicit-any': 'warn',
     // 절대 경로로 작성 되어 있는지 체크
     'no-relative-import-paths/no-relative-import-paths': [
       'warn',
