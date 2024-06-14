cf7069e554a0b3c77554cc0ec100fdd1b44a278b
intersoom <sallybig@naver.com>
2024-06-14 16:13:04 +0900

    fix: remove unnecessary code

--- apps/example/config-example/src/App.tsx
+++ apps/example/config-example/src/App.tsx
@@ -2,11 +2,10 @@
 
 const App = () => {
   // eslint-config 동작 검증을 위해서 주석을 풀어주세요 !
-
-  // const [count, setCount] = useState(0);
+  // const [count] = useState(0);
   // useEffect(() => {
   //   console.log(count);
-  // }, []);
+  // }, [count]);
   return <></>;
 };
 
