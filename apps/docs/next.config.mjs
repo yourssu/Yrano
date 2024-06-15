52ba7bb5fad378edd0d83e33319bf66f17f2b209
심민성 <bighammer1753@gmail.com>
2024-06-15 22:46:25 +0900

    docs: 기존 md파일 nextra docs로 이전

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -11,4 +11,8 @@ const withNextra = nextra({
 
 export default withNextra({
   reactStrictMode: true,
+  i18n: {
+    locales: ['en', 'ko'],
+    defaultLocale: 'en',
+  },
 });
--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -3,10 +3,14 @@ import React from 'react';
 import { DocsThemeConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Yrano</span>,
   footer: {
     text: 'Nextra Docs Template',
   },
+  i18n: [
+    { locale: 'en', text: 'English' },
+    { locale: 'ko', text: '한국어' },
+  ],
 };
 
 export default config;
