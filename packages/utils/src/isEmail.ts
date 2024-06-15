89ed5bd70f39ee5a724e230303ea1e626cb3bb83
intersoom <sallybig@naver.com>
2024-06-15 20:59:28 +0900

    feat : create isEmail

---
+++ packages/utils/src/isEmail.ts
@@ -0,0 +1,19 @@
+export const isEmail = (email: string, domain?: string) => {
+  const regFull =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (regFull.test(String(email).toLowerCase())) {
+    return email;
+  }
+
+  const regHalf = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))$/;
+  const refDomain =
+    /^@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (domain) {
+    if (regHalf.test(String(email).toLowerCase()) && refDomain.test(String(domain).toLowerCase())) {
+      return email + domain;
+    }
+  }
+
+  return false;
+};
